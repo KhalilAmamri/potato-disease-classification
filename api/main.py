c7bda94ec60e0fd19996a81a70782b3b0603372c
KhalilAmamri <KhalilAmamri@outlook.com>
2025-10-03 00:30:27 +0100

    feat: add predict endpoint to FastAPI application for file uploads

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
 
 app = FastAPI()
@@ -6,6 +6,13 @@ app = FastAPI()
 @app.get("/ping")
 async def ping():
     return {"Hello, I am alive!"}
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    pass
+    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
